2de21578d0cf762c42a34303d4083677ee866a72
Collins Omwenga <comwenga@live.com>
2024-08-10 11:07:56 +0300

    Implemented Q1-Section1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        // Read a list of integers from the console
+        Console.WriteLine("Enter numbers separated by space:");
+        var numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+        // Remove duplicates using a HashSet and then convert back to a list
+        var uniqueNumbers = new HashSet<int>(numbers).ToList();
+
+        // Sort the list in descending order
+        uniqueNumbers.Sort((a, b) => b.CompareTo(a));
+
+        // Print the sorted list
+        Console.WriteLine("Sorted list in descending order without duplicates:");
+        uniqueNumbers.ForEach(Console.WriteLine);
+    }
+}
